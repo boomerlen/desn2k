962ace8164b0105ce75855dcf987de3f5b53ef21
Hugo Sebesta <bunnieweirdo@gmail.com>
2025-07-25 16:25:50 +1000

    add clock.c

---
+++ platform/src/clock.c
@@ -0,0 +1,23 @@
+#include "clock.h"
+
+struct clock_status clock;
+
+struct clock_status *setup_clock() {
+    return &clock;
+}
+
+void set_time() {
+
+}
+
+void get_time() {
+
+}
+
+void set_alert() {
+
+}
+
+void alert() {
+
+}
