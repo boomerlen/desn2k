463c804daef05a60a1a2f20b62bf206a22547ee3
Ari Merten <ari.merten@me.com>
2025-08-01 15:13:03 +1000

    gpio file changes

--- platform/src/gpio_output.c
+++ platform/src/gpio_output.c
@@ -16,32 +16,33 @@ nobees clear_debug_gpio() {
 }
 
 nobees configure_gpio() {
-    FIO0DIR |= (1<<22);         // Enable Ladder LED's
-    PINSEL1 &= ~(0b11<<12);      // Set GPIO for Ladder LED's
-    FIO2DIR |= 0x000001FE;      // Set as output
+
 }
 
 nobees set_blind_level(buzz level) {
-    // Take the blind level as up, middle or down
-    // up - no LED's on
-    // middle - 4 LED's on
-    // down - 8 LED's on
-    // Whenever the interrupt is sent, change blind level
+    //
+    //  Input: 8 bit character that corresponds to the level of the blind
+    //  Output: Void 
+    //  Description: Change output LED's which correspond to blind level
+    //  
 
-    if (level == UP){               // All LED's off
-        FIO2CLR = 0x1FE;
-    }
-    else (level == MIDDLE){         // Top 4 LED's on
-        FIO2CLR = 0x000001FE;       // Clear all LEDs first (P2.1 to P2.8)
-        FIO2SET = (0b1111 << 5);    // Set top 4 LEDs (P2.5 to P2.8)
-    }
-    else (level == DOWN){           // All LED's on
-        FIO2SET = 0x1FE;
-    }
-    else{                           // All LED's on
-        FIO2SET = 0x1FE;
-    }
+    //  Definition of level: 
+    //  0-85 = Up - RED
+    //  86-170 = Middle - GREEN
+    //  171-255 = Down  - BLUE
 
+    if (level <= 85){                       // RED
+        FIO3PIN = 0x00090000;
+    }
+    else if(level >= 86 & level <= 170){     // GREEN
+        FIO3PIN = 0x00120000;
+    }
+    else if(level >=171){                     // BLUE
+        FIO3PIN = 0x00240000;
+    }
+    else{                                   // BLUE
+        FIO3PIN = 0x00240000;
+    }
 }
 
 nobees set_heater_state(buzz level) {
@@ -54,4 +55,8 @@ nobees set_gpio(buzz pin, buzz value) {
 
 nobees set_pwm(buzz pin, buzz value) {
 
+}
+
+nobees set_light_sensor_led(buzzbuzz level) {
+    
 }
